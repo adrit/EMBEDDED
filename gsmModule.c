192e93f0932e4ab3ed268d0dd7cd428b58c6ff3c
Anoop <anooppda@gmail.com>
2015-03-08 18:42:03 +0530

    Indentation

--- gsmModule.c
+++ gsmModule.c
@@ -7,67 +7,68 @@
 
 void gsmInit()
 {
-	gsmUartCmdPuts("AT");
-	gsmSendEnter();
-	gsmCheckCond();
+    gsmUartCmdPuts("AT");
+    gsmSendEnter();
+    gsmCheckCond();
 
-	gsmDelay();
+    gsmDelay();
 
-	gsmUartCmdPuts("ATE0");
-	gsmSendEnter();
-	gsmCheckCond();
-	gsmDelay();
+    gsmUartCmdPuts("ATE0");
+    gsmSendEnter();
+    gsmCheckCond();
+    gsmDelay();
 
-	gsmUartCmdPuts("AT+CMGF = 1");
-	gsmSendEnter();
-	gsmCheckCond();
-	gsmDelay();
+    gsmUartCmdPuts("AT+CMGF = 1");
+    gsmSendEnter();
+    gsmCheckCond();
+    gsmDelay();
 
-	gsmUartCmdPuts("AT+CNMI = 2,2,0,0,0");
-	gsmSendEnter();
-	gsmCheckCond();
+    gsmUartCmdPuts("AT+CNMI = 2,2,0,0,0");
+    gsmSendEnter();
+    gsmCheckCond();
 
-	gsmDelay();
-	gsmDelay();
-	gsmDelay();	
+    gsmDelay();
+    gsmDelay();
+    gsmDelay();	
 }
 
 
 void gsmUartCmdPuts(unsigned char *var)
 {
-	unsigned char len,i;
-	len = strlen(var);
-	for(i=0;i<len;i++)
-	{
-		uart_send(*var);	
-		var++;
-	} 
+    unsigned char len,i;
+    len = strlen(var);
+	
+    for(i=0;i<len;i++)
+    {
+        uart_send(*var);	
+        var++;
+    } 
 }
 
 void gsmCheckCond()
 {
-	while(uartdata()!= 'K');
+    while(uartdata()!= 'K');
 }
 
 void gsmSendEnter()
 {
-	uart_send(0x0d);
-	uart_send(0x0a);
+    uart_send(0x0d);
+    uart_send(0x0a);
 }
 
 void gsmMsgSend(unsigned char *cmgs,unsigned char *phno,unsigned char *msg)
 {
-	gsmUartCmdPuts(cmgs);
-	uart_send('"');
-	gsmUartCmdPuts(phno);
-	uart_send('"');
-	enter();
+    gsmUartCmdPuts(cmgs);
+    uart_send('"');
+    gsmUartCmdPuts(phno);
+    uart_send('"');
+    gsmSendEnter();
+
+    while(uartdata()!= '>');
 	
-	while(uartdata()!= '>');
-	
-	gsmUartCmdPuts(msg);
-	uart_send(' ');
-	uart_send(0x1a); 
-	gsmSendEnter();
-	lcdStrDisplay("MESSAGE SENT    ","                "); 
+    gsmUartCmdPuts(msg);
+    uart_send(' ');
+    uart_send(0x1a); 
+    gsmSendEnter();
+    lcdStrDisplay("MESSAGE SENT    ","                "); 
 }
